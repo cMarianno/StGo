e1bc01e74e4f34ba8098aa50ca2e86b90e68dd96
cMarianno <caiosadao@gmail.com>
2022-07-27 08:55:52 -0300

    Lesson 1.2 - Scan, Scanf

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
-	fmt.Println("Caio Mariano")
+	nome := "Caio"
+	idade := 24
+	versao := 1.2
+	fmt.Println("Olá sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa está na versão", versao)
+	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(nome))
+
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
+
+	var comando int
+	// fmt.Scanf("%d", &comando)
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da minha variavel comando é", &comando)
+	fmt.Println("O comando escolhido foi", comando)
 }
