416503b3b534575391327e291bb23aa6f50a7960
cMarianno <caiosadao@gmail.com>
2022-08-05 17:07:35 -0300

    If - Swith - Exit Error - Function - Exit Success

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -2,24 +2,65 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"os"
 )
 
 func main() {
-	nome := "Caio"
-	idade := 24
-	versao := 1.2
-	fmt.Println("Olá sr.", nome, "sua idade é", idade)
+	/* Call Function without Return */
+	exibeIntroducao()
+	exibeMenu()
+	/* Call Function with Return */
+	comando := leComando()
+
+	/*
+		If
+			if comando == 1 {
+				fmt.Println("Monitorando...")
+			} else if comando == 2 {
+				fmt.Println("Exibindo Logs...")
+			} else if comando == 0 {
+				fmt.Println("Saindo do programa...")
+			} else {
+				fmt.Println("Não conheço este comando")
+			}
+	*/
+
+	/* Swith */
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		/* Exit Success */
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		/* Exit Error */
+		os.Exit(-1)
+	}
+}
+
+/* Function without Return */
+func exibeIntroducao() {
+	nome := "Douglas"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
-	fmt.Println("O tipo da variavel nome é", reflect.TypeOf(nome))
+}
 
-	fmt.Println("1 - Iniciar Monitoramento")
-	fmt.Println("2 - Exibir Logs")
-	fmt.Println("0 - Sair do Programa")
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
 
-	var comando int
-	// fmt.Scanf("%d", &comando)
-	fmt.Scan(&comando)
-	fmt.Println("O endereço da minha variavel comando é", &comando)
-	fmt.Println("O comando escolhido foi", comando)
+/* Function with Int Return */
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
